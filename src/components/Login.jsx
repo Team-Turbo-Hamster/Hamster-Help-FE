c09507a11128dc42fc3925079df9c9014c5141cc
Jack <jcpp137@gmail.com>
2022-01-25 15:35:58 +0000

    login page updating to state then sending on commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 
 const InitialLogin = () => {
   const [values, setValues] = useState({ username: "", password: "", showPassword: false });
-  const handleChange = (event) => {
+  
+  const handleChange = (prop) => (event) => {
     event.preventDefault();
-    console.log(event);
-    setValues({ ...values, username: event.target[0].value, password: event.target[2].value });
+    setValues({ ...values, [prop]: event.target.value, });
   };
 
   const handleClickShowPassword = () => {
@@ -21,18 +21,25 @@ const InitialLogin = () => {
     event.preventDefault();
   };
 
+  const handleSubmit = (event) => {
+    console.log("submit: ", values)
+    console.log(event)
+    setValues({ ...values, username: event.target[0].value, password: event.target[2].value,});
+    event.preventDefault()
+  }
+
   return (
-    <form onSubmit={handleChange}>
-      <TextField helperText="Please enter your username" label="User" placeholder="Name" />
+    <form onSubmit={handleSubmit}>
+      <TextField helperText="Please enter your username" label="User" placeholder="Name" onChange={handleChange('username')}  />
       <InputLabel htmlFor="passwordField">Password</InputLabel>
       <FilledInput
         id="passwordField"
         type={values.showPassword ? "text" : "password"}
         value={values.password}
+        onChange={handleChange('password')}
         endAdornment={
           <InputAdornment position="end">
             <IconButton
-              onChange={handleChange}
               aria-label="toggle password visibility"
               onClick={handleClickShowPassword}
               onMouseDown={handleMouseDownPassword}
@@ -44,7 +51,7 @@ const InitialLogin = () => {
         }
       />
       <Button variant="text" type="Submit">
-        Submit
+        Submit 
       </Button>
     </form>
   );
