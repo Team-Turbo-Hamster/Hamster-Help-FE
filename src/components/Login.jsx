0884c57be41bdc92670e8566a98e5dc673f8c739
Hippy <barhippy@googlemail.com>
2022-01-25 09:37:07 +0000

    add password field to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,16 +3,19 @@ import { useState } from "react";
 
 const InitialLogin = () => {
   const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
   const handleChange = (event) => {
     event.preventDefault();
-    console.log(event.target[0].value);
-    setUserName(event.target.value);
+    console.log(event);
+    setUserName(event.target[0].value);
+    setUserPassword(event.target[2].value);
     // return userName;
   };
   return (
     <form onSubmit={handleChange}>
       <TextField helperText="Please enter your username" label="User" placeholder="Name" />
+      <TextField helperText="Please enter your password" label="Password" placeholder="Password" id="password" />
       <Button variant="text" type="Submit">
         Submit
       </Button>
