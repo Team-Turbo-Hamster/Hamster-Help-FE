465d3cbf1d427f1e3b62ea93bda732f60eddd570
Jack <jcpp137@gmail.com>
2022-01-25 17:11:05 +0000

    /register roite added, New User registration form begun

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import Tutor from "./pages/Tutor";
 import CloudinaryTest from "./pages/CloudinaryTest";
 import Login from "./pages/Login.page";
+import NewUser from "./pages/NewUser.page";
 
 function App() {
  // const {}= useAuth()
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<NewUser />} />
         <Route path="tutor" element={<Tutor />} />
         <Route path="cloudinary-test" element={<CloudinaryTest />} />
       </Route>
