2796a6984b0769a5219bab4e4a52edd00688699a
Hippy <barhippy@googlemail.com>
2022-01-25 11:47:42 +0000

    password field added - not functioning

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,15 @@
-import { Button, TextField, FormControl, InputLabel, FilledInput, InputAdornment, IconButton } from "@mui/material";
-import {Visibility, VisibilityOff} from '@mui/icons-material';
+import { Button, TextField, InputLabel, FilledInput, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useState } from "react";
 
 const InitialLogin = () => {
-  const [values, setValues] = useState({username: "", password:"", showPassword: false});
-
-
+  const [values, setValues] = useState({ username: "", password: "", showPassword: false });
   const handleChange = (event) => {
     event.preventDefault();
     console.log(event);
-    setValues({...values, username: event.target[0].value, password: event.target[2].value});
-  
+    setValues({ ...values, username: event.target[0].value, password: event.target[2].value });
   };
-    
+
   const handleClickShowPassword = () => {
     setValues({
       ...values,
@@ -23,46 +20,34 @@ const InitialLogin = () => {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-  
-  
-  return (
-    // <form onSubmit={handleChange}>
-    <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
-        console.log(event);
-      <TextField helperText="Please enter your username" label="User" placeholder="Name" />
-        <InputLabel htmlfor="passwordField">Password</InputLabel>
-        <FilledInput
-          id="passwordField"
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-           </InputAdornment>
-          }
-        />
 
+  return (
+    <form onSubmit={handleChange}>
+      <TextField helperText="Please enter your username" label="User" placeholder="Name" />
+      <InputLabel htmlFor="passwordField">Password</InputLabel>
+      <FilledInput
+        id="passwordField"
+        type={values.showPassword ? "text" : "password"}
+        value={values.password}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              onChange={handleChange}
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {values.showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
       <Button variant="text" type="Submit">
         Submit
       </Button>
-      </FormControl>
-
-    // </form>
+    </form>
   );
 };
-
-
-
-    
-     
-
 
 export default InitialLogin;
