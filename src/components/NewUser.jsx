08d417b3f73a21a03e2fb48c06f974634d01b3ba
Jack <jcpp137@gmail.com>
2022-01-26 11:47:36 +0000

    added regex verification but not functioning

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,16 +1,25 @@
 import { Button, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NewUserForm = () => {
-
-  console.log("i'm alive!")
   const [values, setValues] = useState({ username: "", password: "", passwordVerify: "", showPassword: false });
-  
+  const [tests, setTests] = useEffect({username: /^[a-zA-Z\s-]{1,30}$/, password: /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])^[a-zA-Z0-9!@#$%^&*]{6,30}$/, passwordVerify: ""})
+  const [testsPassed, setTestsPassed] = useEffect({})
+
   const handleChange = (prop) => (event) => {
     event.preventDefault();
     setValues({ ...values, [prop]: event.target.value, });
 
+  };
+
+  const handleBlur = (prop) => (event) => {
+    console.log(prop)
+    console.log(tests.prop)
+    // let test = new RegExp(tests.prop)
+
+    // test.test(values.prop) ? setTestsPassed(...testsPassed, [prop] = true) : setTestsPassed(...testsPassed, [prop] = false);
+    console.log(testsPassed)
   };
 
   const handleClickShowPassword = () => {
@@ -40,7 +49,7 @@ const NewUserForm = () => {
 {/* _______________________________________________________ */}
     <FormControl sx={{ m: 1, width: '30ch' }}>
     <InputLabel>Username</InputLabel>
-      <OutlinedInput id="usernameField" label="Username" helpertext="Please enter your username" onChange={handleChange('username')}  />
+      <OutlinedInput id="usernameField" label="Username" helpertext="Please enter your username" onBlur={handleBlur} onChange={handleChange('username')}  />
       </FormControl><br />
        {/* Password field */}
 {/* _______________________________________________________ */}
