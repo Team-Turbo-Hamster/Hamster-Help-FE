f0b69543705feb50b7fec9a5f43ac7662df6481f
Jack <jcpp137@gmail.com>
2022-02-01 11:05:32 +0000

    chip word cloud and validation on login, register and new ticket

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
+  Alert
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useState } from "react";
@@ -17,13 +18,37 @@ const Login = () => {
     password: "Qweqweqwe2",
   });
   const [showPassword, setShowPassword] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [testsPassed, setTestsPassed] = useState({username:true, password:true});
+  const helperText = {username:"Username must be 1-30 character long and contain only letter or spaces", password:"Password must be 6-30 characters long, must contain only letters, numbers and special characters(!@#$%^&*) and must contain at least one uppercase letter, one lowercase letter and one number"}
+  const tests =  {username: /^[a-zA-Z\s-]{4,15}$/, password: /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])^[a-zA-Z0-9!@#$%^&*]{6,30}$/,};
 
   const { signIn, user } = useAuth();
 
   const handleChange = (prop) => (event) => {
     event.preventDefault();
     setValues({ ...values, [prop]: event.target.value });
+    let test = new RegExp(tests[prop]);
+    test.test(values[prop])
+    ? setTestsPassed({ ...testsPassed, [prop]: true })
+    : setTestsPassed({ ...testsPassed, [prop]: false });
+    console.log(testsPassed)
+    testsPassed.username && testsPassed.password ? setButtonDisabled(false) :  setButtonDisabled(true)
   };
+
+  const handleBlur = (prop) => (event) => {
+    let test = new RegExp(tests[prop]);
+    test.test(values[prop])
+    ? setTestsPassed({ ...testsPassed, [prop]: true })
+    : setTestsPassed({ ...testsPassed, [prop]: false });
+    testsPassed.username && testsPassed.password ? setButtonDisabled(false) :  setButtonDisabled(true)
+
+    console.log(testsPassed)
+   } 
+
+   const HelperText = (prop) => {
+    return testsPassed[prop] === true ? <></> : (<Alert severity="error">{helperText[prop]}</Alert>)  
+   }
 
   const handleClickShowPassword = () => {
     setShowPassword((prevState) => !prevState);
@@ -52,7 +77,9 @@ const Login = () => {
           value={values.username}
           helpertext="Please enter your username"
           onChange={handleChange("username")}
+          onBlur={handleBlur("username")}
         />
+      {HelperText("username")}
       </FormControl>
       <br />
       <FormControl sx={{ m: 1, width: "30ch" }}>
@@ -63,6 +90,7 @@ const Login = () => {
           value={values.password}
           label="password"
           onChange={handleChange("password")}
+          onBlur={handleBlur("password")}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -76,9 +104,10 @@ const Login = () => {
             </InputAdornment>
           }
         />
+         {HelperText("password")}
       </FormControl>
       <br />
-      <Button variant="text" type="Submit">
+      <Button variant="text" type="Submit" disabled={buttonDisabled}>
         Submit
       </Button>
     </form>
